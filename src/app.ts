929a829a1aec3aaee32b6c95893052ea87d3edba
James Jhong <jamesjhong@star-link.tech>
2023-05-21 04:48:55 +0800

    add lint-staged & husky

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express from "express"
-import { createServer } from "http"
-import { Server } from "socket.io"
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-app.get("/", (req, res) => {
-  return res.send("hello world")
+app.get('/', (req, res) => {
+  return res.send('hello world')
 })
 
-io.on("connection", () => {
-  console.log("connect")
+io.on('connection', () => {
+  console.log('connect')
 })
 
 server.listen(3000, () => {
-  console.log("API is linstening on port 3000")
+  console.log('API is linstening on port 3000')
 })
