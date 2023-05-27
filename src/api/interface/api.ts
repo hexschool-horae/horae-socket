f121a9bf99b8e8dc2f9e73557d17031e7f25f9e3
James Jhong <jamesjhong@star-link.tech>
2023-05-27 20:44:11 +0800

    add board tag interface

--- src/api/interface/api.ts
+++ src/api/interface/api.ts
@@ -7,3 +7,29 @@ export interface IPostBoardListRequest {
   boardId: string
   token: string
 }
+
+export interface IGetBoardTagsByBoardIdRequest {
+  boardId: string
+  token: string
+}
+
+export interface IPostBoardTagsRequest {
+  title: string
+  color: string
+  boardId: string
+  token: string
+}
+
+export interface IPutBoardTagsRequest {
+  tagId: string
+  title: string
+  color: string
+  boardId: string
+  token: string
+}
+
+export interface IDeleteBoardTagsRequest {
+  tagId: string
+  boardId: string
+  token: string
+}
