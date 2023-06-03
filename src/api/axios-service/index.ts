a491157712cb2c7612654ff0e34100fcde653972
James Jhong <jamesjhong@star-link.tech>
2023-06-04 06:00:06 +0800

    add new axios service

--- src/api/axios-service/index.ts
+++ src/api/axios-service/index.ts
@@ -2,14 +2,54 @@ import axios from '../axios'
 import apiPath from '../path'
 import * as interfaces from '../interface/api'
 
-export const GET_BOARD_BY_BOARD_ID = (payload: interfaces.IGetBoardByIdRequest) => {
-  const url = apiPath.GET_BOARD_BY_BOARD_ID.replace(':board-id', payload.boardId)
-  return axios.get<interfaces.IGetBoardByIdRequest>(url, payload)
+export const PATCH_BOARD_VIEW_SET = (payload: interfaces.IModifyBoardViewPermissionRequest) => {
+  const url = apiPath.PATCH_BOARD_VIEW_SET_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.post(url, payload)
 }
 
-export const POST_BOARD_LIST_BY_ID = (payload: interfaces.IPostBoardListRequest) => {
-  const url = apiPath.POST_BOARD_LIST_BY_ID.replace(':board-id', payload.boardId)
-  return axios.post<interfaces.IPostBoardListRequest>(url, payload)
+export const PATCH_BOARD_STATUS_BY_BOARD_ID = (payload: interfaces.IPatchBoardStatusByBoardIdRequest) => {
+  const url = apiPath.PATCH_BOARD_STATUS_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.patch(url, payload)
+}
+
+export const PATCH_BOARD_TITLE_BY_BOARD_ID = (payload: interfaces.IPatchBoardTitleByBoardIdRequest) => {
+  const url = apiPath.PATCH_BOARD_TITLE_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.patch(url, payload)
+}
+
+export const GET_BOARD_BY_BOARD_ID = (payload: interfaces.IGetBoardByIdRequest) => {
+  const url = apiPath.GET_BOARD_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.get(url, payload)
+}
+
+export const POST_BOARD_LIST_BY_BOARD_ID = (payload: interfaces.IPostBoardListRequest) => {
+  const url = apiPath.POST_BOARD_LIST_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.post(url, payload)
+}
+
+export const PATCH_LIST_TITLE_BY_LIST_ID = (payload: interfaces.IPatchBoardListTitleByListIdRequest) => {
+  const url = apiPath.PATCH_LIST_TITLE_BY_LIST_ID.replace(':list-id', payload.listId)
+  return axios.patch(url, payload)
+}
+
+export const PATCH_LIST_STATUS_BY_LIST_ID = (payload: interfaces.IPatchBoardListStatusByListIdRequest) => {
+  const url = apiPath.PATCH_LIST_STATUS_BY_LIST_ID.replace(':list-id', payload.listId)
+  return axios.patch(url, payload)
+}
+
+export const POST_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IPostBoardTagsRequest) => {
+  const url = apiPath.POST_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.post(url, payload)
+}
+
+export const PUT_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IPutBoardTagsRequest) => {
+  const url = apiPath.PUT_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.put(url, payload)
+}
+
+export const DELETE_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IDeleteBoardTagsRequest) => {
+  const url = apiPath.DELETE_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
+  return axios.delete(url, payload)
 }
 
 export const GET_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IGetBoardTagsByBoardIdRequest) => {
@@ -17,22 +57,22 @@ export const GET_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IGetBoardTagsByBo
   return axios.get<interfaces.IGetBoardTagsByBoardIdRequest>(url, payload)
 }
 
-export const POST_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IPostBoardTagsRequest) => {
-  const url = apiPath.POST_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
-  return axios.post<interfaces.IPostBoardTagsRequest>(url, payload)
-}
-
-export const PUT_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IPutBoardTagsRequest) => {
-  const url = apiPath.PUT_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
-  return axios.put<interfaces.IPutBoardTagsRequest>(url, payload)
-}
-
-export const DELETE_BOARD_TAGS_BY_BOARD_ID = (payload: interfaces.IDeleteBoardTagsRequest) => {
-  const url = apiPath.DELETE_BOARD_TAGS_BY_BOARD_ID.replace(':board-id', payload.boardId)
-  return axios.delete<interfaces.IDeleteBoardTagsRequest>(url, payload)
-}
-
-export const POST_LIST_CARD_BY_ID = (payload: interfaces.IPostBoardCardByListIdRequest) => {
+export const POST_LIST_CARD_BY_LIST_ID = (payload: interfaces.IPostBoardCardByListIdRequest) => {
   const url = apiPath.POST_LIST_CARD_BY_LIST_ID.replace(':list-id', payload.listId)
-  return axios.post<interfaces.IPostBoardCardByListIdRequest>(url, payload)
+  return axios.post(url, payload)
+}
+
+export const PATCH_CARD_BY_CARD_ID = (payload: interfaces.IPatchCardByCardIdRequest) => {
+  const url = apiPath.PATCH_CARD_BY_CARD_ID.replace(':card-id', payload.cardId)
+  return axios.patch(url, payload)
+}
+
+export const POST_CARD_TAG_BY_CARD_ID = (payload: interfaces.IPostCardTagByCardIdRequest) => {
+  const url = apiPath.PATCH_CARD_BY_CARD_ID.replace(':card-id', payload.cardId)
+  return axios.patch(url, payload)
+}
+
+export const DELETE_CARD_TAG_BY_CARD_ID = (payload: interfaces.IDeleteCardTagByCardIdRequest) => {
+  const url = apiPath.DELETE_CARD_TAG_BY_CARD_ID.replace(':card-id', payload.cardId)
+  return axios.patch(url, payload)
 }
