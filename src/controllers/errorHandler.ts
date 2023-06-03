1dd9153561e27018c47dc6ba8a51b62017eef4c6
James Jhong <jamesjhong@star-link.tech>
2023-06-04 05:59:39 +0800

    修改 error handler

--- src/controllers/errorHandler.ts
+++ src/controllers/errorHandler.ts
@@ -2,9 +2,15 @@ import { AxiosError } from 'axios'
 import { Socket } from 'socket.io'
 const handlerError = (error: AxiosError | Error, socket: Socket, errorEvent: string) => {
   if (error instanceof AxiosError) {
-    socket.emit(errorEvent, error.response?.data)
+    socket.emit(errorEvent, {
+      code: -1,
+      data: error.response?.data,
+    })
   } else {
-    socket.emit(errorEvent, error)
+    socket.emit(errorEvent, {
+      code: -1,
+      error,
+    })
   }
 }
 
